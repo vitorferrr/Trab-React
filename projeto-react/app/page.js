1ce37a7e26d7fc891b96c862939f18abcfbb0d14
Vitor Ferreira <vitorferr1412@gmail.com>
2026-03-10 01:06:49 +0000

    remoção da pasta assets após complicação, acerto da imagem no Profile e inserção dos textos pedidos

--- projeto-react/app/page.js
+++ projeto-react/app/page.js
@@ -2,7 +2,13 @@ import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl">
+        Site criado para a disciplina Programação Front-End
+      </h1>
+
+      <hr />
+      
       <Profile />
     </div>
   );
--- projeto-react/components/Profile.js
+++ projeto-react/components/Profile.js
@@ -1,10 +1,20 @@
-import fotoPerfil from '../assets/image-profile.jpeg';
-
-export default function Profile() {
+export default function Profile({size = 200}) {
   return (
-    <img
-      src={fotoPerfil}
-      alt="Vitor Ferreira"
-    />
+    <div className="flex flex-col items-center text-center">
+      <img 
+        src="https://media.licdn.com/dms/image/v2/D4D03AQGtKejMt85GHQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727647494822?e=1774483200&v=beta&t=pYZzgE_F1VoxSt3tTJRrzC9_MX2Ochq5IxNlAvfZvgg"
+        alt="Vitor Ferreira" 
+        width={size} 
+        height={size}
+      />
+
+      <h2>
+        Vitor dos Santos Ferreira
+      </h2>
+
+      <p>
+        Estudante de Sistemas para Internet na UNICAP. Estou aprendendo React/NextJS.
+      </p>
+    </div>
   )
 }
