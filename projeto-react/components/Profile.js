4b98cbe8aa43ae197fe64a33e581694666873521
Vitor Ferreira <vitorferr1412@gmail.com>
2026-03-10 00:22:45 +0000

    Criação de uma pasta assets para importar uma imagem e criação do arquivo Profile dentro da pasta components

---
+++ projeto-react/components/Profile.js
@@ -0,0 +1,10 @@
+import fotoPerfil from '../assets/image-profile.jpeg';
+
+export default function Profile() {
+  return (
+    <img
+      src={fotoPerfil}
+      alt="Vitor Ferreira"
+    />
+  )
+}
